8ea8472a80448f9fae4a36b7ec305d24c5a44dff
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-01-10 15:25:57 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Routes from './routes';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Routes />
     </div>
   );
 }
